40ff354cadcdd722461f32b19cb6dbd77e15c62d
dariel <annas_ramadhani@yahoo.co.id>
2012-05-02 16:34:32 +0700

    fix map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,13 @@ class HomeController < ApplicationController
   def index
     @geo = SimpleGeolocation::Geocoder.new(request.ip.to_s)
     @geo.geocode!
-
-#    @search = Bar.search(params[:search])
+    #    @search = Bar.search(params[:search])
+    #    @bars = @search.all
     @search = Swig.search(params[:search])
-    @bars = @search.all
-    @swigs = @search.where(status: "active")
+    if !params[:search].nil?
+      @swigs = @search.where(status: "active")
+    else
+      @swigs = @search.joins("INNER JOIN bars ON swigs.bar_id = bars.id").where(["bars.city = ?", @geo.city])
+    end
   end
 end
