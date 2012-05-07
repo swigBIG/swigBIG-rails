cc937253ea8d62f945a2fdbe3ed9b440a90159d8
dariel <annas_ramadhani@yahoo.co.id>
2012-05-07 18:39:56 +0700

    fixed bar

--- app/controllers/bar_detail_controller.rb
+++ app/controllers/bar_detail_controller.rb
@@ -4,8 +4,8 @@ class BarDetailController < ApplicationController
     @bar = Bar.find(params[:id])
     @swigs = Swig.where(status: "active")
     @swigers = @bar.swigers.all
-    @popularity = @bar.rewards.where(reward_type: "Popularity")
-    @loyalty = @bar.rewards.where(reward_type: "Loyalty")
+    @popularity = Popularity.where(bar_id: @bar)
+    @loyalty = Loyalty.where(bar_id: @bar)
   end
 
   def bar_swig
