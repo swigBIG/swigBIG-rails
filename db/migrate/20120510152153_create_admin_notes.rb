cc4c8f1205423a6bc3898ff9bc6fcdb98da2a2b4
dariel <annas_ramadhani@yahoo.co.id>
2012-05-10 18:40:21 +0700

    fixed data

--- db/migrate/20120510152153_create_admin_notes.rb
+++ db/migrate/20120510152153_create_admin_notes.rb
@@ -1,8 +1,8 @@
 class CreateAdminNotes < ActiveRecord::Migration
   def self.up
     create_table :admin_notes do |t|
-      t.string :resource_id, :null => false
-      t.string :resource_type, :null => false
+      t.string :resource_id, :null => false, :limit => (255)
+      t.string :resource_type, :null => false, :limit => (255)
       t.references :admin_user, :polymorphic => true
       t.text :body
       t.timestamps
