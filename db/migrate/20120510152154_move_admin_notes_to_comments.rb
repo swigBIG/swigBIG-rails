04afc53eafb308d66c0b4337cd4f64eab6ab44e0
dariel <annas_ramadhani@yahoo.co.id>
2012-05-10 19:23:57 +0700

    fixed date

--- db/migrate/20120510152154_move_admin_notes_to_comments.rb
+++ db/migrate/20120510152154_move_admin_notes_to_comments.rb
@@ -1,12 +1,12 @@
 class MoveAdminNotesToComments < ActiveRecord::Migration
   def self.up
-    remove_index  :admin_notes, [:admin_user_type, :admin_user_id]
+#    remove_index  :admin_notes, [:admin_user_type, :admin_user_id]
     rename_table  :admin_notes, :active_admin_comments
     rename_column :active_admin_comments, :admin_user_type, :author_type
     rename_column :active_admin_comments, :admin_user_id, :author_id
     add_column    :active_admin_comments, :namespace, :string
-    add_index     :active_admin_comments, [:namespace]
-    add_index     :active_admin_comments, [:author_type, :author_id]
+#    add_index     :active_admin_comments, [:namespace]
+#    add_index     :active_admin_comments, [:author_type, :author_id]
 
     # Update all the existing comments to the default namespace
     say "Updating any existing comments to the #{ActiveAdmin.application.default_namespace} namespace."
