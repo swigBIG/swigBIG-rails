613b2142feab4bc89a611323d842eae9e9e5cc5f
dariel <annas_ramadhani@yahoo.co.id>
2012-05-16 19:40:07 +0700

    adding sport team

---
+++ app/admin/sport_teams.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register SportTeam do
+  filter :team_name
+  filter :category
+
+  index do
+    column :team_name
+    column :category
+    default_actions
+  end
+
+  show do |b|
+
+    attributes_table do
+      row :team_name do
+        b.team_name
+      end
+      row :category do
+        b.category
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs "Adding Team" do
+      f.input :team_name
+      f.input :category
+    end
+    f.buttons
+  end
+end
