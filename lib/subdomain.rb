e51c0212cb4a954610f3a49a51ca0938f7793c79
Syafik <syafik@syafik.(none)>
2012-05-18 10:42:09 +0700

    subdomain changes

--- lib/subdomain.rb
+++ lib/subdomain.rb
@@ -1,7 +1,7 @@
 class Subdomain
   def self.matches?(request)
     case request.host.split(/\./).first
-    when 'www', '', nil, 'syafik-aink', 'swigbig', 'swigbig.com', 'angga-dra'
+    when 'www', '', nil, 'syafik-aink', 'swigbig', 'swigbig.com', 'angga-dra', 'http://www'
       false
     else
       true
