f93478470f6be00e10bce2ddc9565965c68611a7
dariel <annas_ramadhani@yahoo.co.id>
2012-05-21 20:00:49 +0700

    fixed facebook login

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+#Rails.application.config.middleware.use OmniAuth::Builder do
+#  provider :facebook, ENV['217608421677403'], ENV['2e93553428ed01d4f5a850b3adc3e70b']
+#
+##  config.omniauth :facebook, "217608421677403", "2e93553428ed01d4f5a850b3adc3e70b"
+#end
