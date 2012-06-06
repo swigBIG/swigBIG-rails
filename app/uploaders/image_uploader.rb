853d124925cf102feb68d0c988409c7c98eafbdf
dariel <annas_ramadhani@yahoo.co.id>
2012-06-06 17:57:51 +0700

    fixed uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -47,6 +47,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [180,180]
   end
 
+  version :mini do
+    process :resize_to_fill => [50,50]
+  end
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
