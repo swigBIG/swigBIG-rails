cde102b3239164e598beaea03119202bb02df01d
dariel <annas_ramadhani@yahoo.co.id>
2012-06-07 11:18:55 +0700

    fixed timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   require 'open-uri'
   before_filter :reject_bot_request, :set_time_zone, :swigbig_content
-
+  include LogActivityStreams
+  
   def reject_bot_request
     user_agent = request.env['HTTP_USER_AGENT']
     if user_agent.include?('bot') or user_agent.include?('spider')
@@ -19,8 +20,9 @@ class ApplicationController < ActionController::Base
         @city_lat_lng = [geo.data['city'], geo.data['latitude'], geo.data['longitude']]
         session["geo_#{set_current_ip}"] = @city_lat_lng
       rescue
-        @city_lat_lng = ["Los Angeles", 34.0863, -118.49]
-        session["geo_75.85.54.184"] = @city_lat_lng
+        #        @city_lat_lng = ["Los Angeles", 34.0863, -118.49]
+        @city_lat_lng = ["Beijing", 39.9289, 116.388]
+        session["geo_211.157.105.218"] = @city_lat_lng
       end
       
      
@@ -29,21 +31,21 @@ class ApplicationController < ActionController::Base
     end
 
     #get timezone
-#    if session["offset_#{set_current_ip}"].blank?
-#      url = URI.parse("http://www.earthtools.org/timezone-1.1/#{@city_lat_lng[1]}/#{@city_lat_lng[2]}")
-#      xml_content = url.open.read
-#      offset = xml_content.scan(/<offset>(.*?)<\/offset>/).first.first rescue nil
-#      session["offset_#{set_current_ip}"] = offset
-#    else
-#      offset = session["offset_#{set_current_ip}"]
-#    end
-#
-#    if offset.blank?
-#      Time.zone = 'Pacific Time (US & Canada)'
-#    else
-#      timezone = ActiveSupport::TimeZone[(offset.to_i)*60*60].name
-#      Time.zone = timezone
-#    end
+    if session["offset_#{set_current_ip}"].blank?
+      url = URI.parse("http://www.earthtools.org/timezone-1.1/#{@city_lat_lng[1]}/#{@city_lat_lng[2]}")
+      xml_content = url.open.read
+      offset = xml_content.scan(/<offset>(.*?)<\/offset>/).first.first rescue nil
+      session["offset_#{set_current_ip}"] = offset
+    else
+      offset = session["offset_#{set_current_ip}"]
+    end
+    
+    if offset.blank?
+      Time.zone = 'Pacific Time (US & Canada)'
+    else
+      timezone = ActiveSupport::TimeZone[(offset.to_i)*60*60].name
+      Time.zone = timezone
+    end
     
   end
 
@@ -55,6 +57,7 @@ class ApplicationController < ActionController::Base
 
   def set_current_ip
     return request.ip.to_s if Rails.env.eql?("development")
-    "75.85.54.184"
+    "211.157.105.218"
+    #    "75.85.54.184"
   end
 end
