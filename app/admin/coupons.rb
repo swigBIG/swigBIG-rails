18ffd4b8ed3369484875acdc69dc3d184be85d22
dariel <annas_ramadhani@yahoo.co.id>
2012-06-08 19:14:47 +0700

    modified coupon

--- app/admin/coupons.rb
+++ app/admin/coupons.rb
@@ -1,3 +1,29 @@
 ActiveAdmin.register Coupon do
-  
+
+  filter :content
+
+  index do
+    column :content
+    default_actions
+  end
+
+  show do |b|
+
+    attributes_table do
+      row :content do
+        b.content
+      end
+      row :coupon_serial do
+        b.coupon_serial
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs "Coupon" do
+      f.input :content
+    end
+    f.buttons
+  end
+
 end
