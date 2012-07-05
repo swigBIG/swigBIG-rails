94cd4ddf6cfc29161b588d9f31c9988798e8560b
dariel <annas_ramadhani@yahoo.co.id>
2012-07-05 18:29:07 +0700

    fixed work hours validations

--- app/models/swiger.rb
+++ app/models/swiger.rb
@@ -9,10 +9,11 @@ class Swiger < ActiveRecord::Base
   #  before_create :check_swiger
   after_create :get_loyalty
 
-  validate :time_and_distance_valid?
+  validate :time_and_distance_valid?, :popularity_reward_valid?
 
-  scope :today, where("created_at >= ? AND created_at  <= ?", Date.today.to_time.in_time_zone.beginning_of_day,  Date.today.to_time.in_time_zone.end_of_day)
+  #  scope :today, where("created_at >= ? AND created_at  <= ?", Date.today.to_time.in_time_zone.beginning_of_day,  Date.today.to_time.in_time_zone.end_of_day)
 
+  scope :today, where("created_at >= ? AND created_at  <= ?", Time.zone.now.beginning_of_day,  Time.zone.now.end_of_day)
 
   #  log_activity_streams self.user_id, :name, "test",
   #    :loyalty_points, :id, :get_loyalty, :swig
@@ -41,12 +42,15 @@ class Swiger < ActiveRecord::Base
 
   def create_activity(actor, object)
     ActivityStream.create(activity: "winloyalty", verb: "Winner Confirmation", actor_id: actor, actor_type: "User", object_id: object, object_type: "Winner")
+    user = User.find(actor)
+    self.bar.send_message(user, {topic: "Loyalty winner", body: "You win Loyalty reward from #{self.bar.name}"})
   end
 
 
   def time_and_distance_valid?
     bar_hour = self.bar.bar_hours.where(day: Time.now.to_time.in_time_zone.strftime("%A")).first
     unless bar_hour.open_time.blank? && bar_hour.close_time.blank?
+#      debugger
       Chronic.time_class = Time.zone
       if (Date.today.to_time.in_time_zone >= Chronic.parse(bar_hour.open_time.gsub(".0",""))) && (Date.today.to_time.in_time_zone <= Chronic.parse(bar_hour.close_time.gsub(".0","")))
         user_swig = self.user.swigers.last
@@ -73,6 +77,22 @@ class Swiger < ActiveRecord::Base
       end
     else
       self.errors.add("time and distance","#{self.bar.name} not set work hours yet!")
+    end
+  end
+
+  def popularity_reward_valid?
+    user_guesses = self.user.popularity_inviters.today.last.users_inviters_id.split(",")
+    today_friends_swiger = self.bar.swigers.today.where(user_id: user_guesses).count
+    unless self.bar.popularity.blank?
+      if self.bar.popularity.swigs_number.eql?(today_friends_swiger)
+        User.where(id: user_guesses).each do |user|
+          self.bar.send_message(user, {topic: "Loyalty winner", body: "You win Loyalty reward from #{self.bar.name}"})
+        end
+      else
+        self.errors.add("popularity reward","#{self.bar.name} popularity not archive yet!!")
+      end
+    else
+      self.errors.add("popularity reward","#{self.bar.name} not create popularity yet!!")
     end
   end
 
