96d14591cb7f059228f6d9930d98491c92cb1e0c
dariel <annas_ramadhani@yahoo.co.id>
2012-08-03 17:37:47 +0700

    add admin page

---
+++ app/admin/site_colors.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register SiteColor do
+  index do |idx|
+    column :nav_bar_color
+    column :background_color
+    default_actions
+  end
+
+  show do |b|
+    attributes_table do
+      row :nav_bar_color do
+        b.nav_bar_color
+      end
+      row :background_color do
+        b.background_color
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs "" do
+      f.input :nav_bar_color, input_html: {class: "color"}
+        f.input :background_color, input_html: {class: "color"}
+          f.buttons
+        end
+      end
+
+    end
