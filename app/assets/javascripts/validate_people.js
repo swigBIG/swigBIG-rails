8da408b52cff0e3169c6ed794264547ea79a650b
dariel <annas_ramadhani@yahoo.co.id>
2012-09-07 11:05:56 +0700

    fixed people validate

--- app/assets/javascripts/validate_people.js
+++ app/assets/javascripts/validate_people.js
@@ -1,37 +1,73 @@
-function validatePeople(day, button){
-  var valid = false;
-
-  if($('#peoples_'+day+'_0').val() == '' || $('#peoples_'+day+'_1').val() == '' || $('#peoples_'+day+'_2').val() == '' || isNaN(parseInt($('#peoples_'+day+'_0').val())) || isNaN(parseInt($('#peoples_'+day+'_1').val())) || isNaN(parseInt($('#peoples_'+day+'_2').val())) || parseInt($('#peoples_'+day+'_0').val()) <= 0 || parseInt($('#peoples_'+day+'_1').val()) <= 0 || parseInt($('#peoples_'+day+'_2').val()) <= 0 ){
-
-    $('#people_warning_text_'+day+'_0').hide();
-    $('#peoples_'+day+'_0').css('border', '1px solid red');
-    $('#people_warning_text_'+day+'_1').hide();
-    $('#peoples_'+day+'_1').css('border', '1px solid red');
-    $('#people_warning_text_'+day+'_2').hide();
-    $('#peoples_'+day+'_2').css('border', '1px solid red');
-    
-    if ($('#peoples_'+day+'_0').val() == '' || isNaN(parseInt($('#peoples_'+day+'_0').val())) || parseInt($('#peoples_'+day+'_0').val()) <= 0 ){
-      $('#people_warning_text_'+day+'_0').show();
+function validatePeople(day, button, action){
+  if (action == "new"){
+    var valid = false;
+    if($('#peoples_'+day+'_0').val() == '' || $('#peoples_'+day+'_1').val() == '' || $('#peoples_'+day+'_2').val() == '' || isNaN(parseInt($('#peoples_'+day+'_0').val())) || isNaN(parseInt($('#peoples_'+day+'_1').val())) || isNaN(parseInt($('#peoples_'+day+'_2').val())) || parseInt($('#peoples_'+day+'_0').val()) <= 0 || parseInt($('#peoples_'+day+'_1').val()) <= 0 || parseInt($('#peoples_'+day+'_2').val()) <= 0 ){
+      $('#people_warning_text_'+day+'_0').hide();
       $('#peoples_'+day+'_0').css('border', '1px solid red');
-    }
-    else if ($('#peoples_'+day+'_1').val() == '' || isNaN(parseInt($('#peoples_'+day+'_1').val())) || parseInt($('#peoples_'+day+'_1').val()) <= 0 ){
-      $('#people_warning_text_'+day+'_1').show();
+      $('#people_warning_text_'+day+'_1').hide();
       $('#peoples_'+day+'_1').css('border', '1px solid red');
-    }
-    else if ($('#peoples_'+day+'_2').val() == '' || isNaN(parseInt($('#peoples_'+day+'_2').val())) || parseInt($('#peoples_'+day+'_2').val()) <= 0 ){
-      $('#people_warning_text_'+day+'_2').show();
+      $('#people_warning_text_'+day+'_2').hide();
       $('#peoples_'+day+'_2').css('border', '1px solid red');
-    }
+    
+      if ($('#peoples_'+day+'_0').val() == '' || isNaN(parseInt($('#peoples_'+day+'_0').val())) || parseInt($('#peoples_'+day+'_0').val()) <= 0 ){
+        $('#people_warning_text_'+day+'_0').show();
+        $('#peoples_'+day+'_0').css('border', '1px solid red');
+      }
+      else if ($('#peoples_'+day+'_1').val() == '' || isNaN(parseInt($('#peoples_'+day+'_1').val())) || parseInt($('#peoples_'+day+'_1').val()) <= 0 ){
+        $('#people_warning_text_'+day+'_1').show();
+        $('#peoples_'+day+'_1').css('border', '1px solid red');
+      }
+      else if ($('#peoples_'+day+'_2').val() == '' || isNaN(parseInt($('#peoples_'+day+'_2').val())) || parseInt($('#peoples_'+day+'_2').val()) <= 0 ){
+        $('#people_warning_text_'+day+'_2').show();
+        $('#peoples_'+day+'_2').css('border', '1px solid red');
+      }
 
-    valid_people = false;
-  }else{
-    $('#peoples_'+day+'_0').css('border', '1px solid grey');
-    $('#peoples_'+day+'_1').css('border', '1px solid grey');
-    $('#peoples_'+day+'_2').css('border', '1px solid grey');
-    valid_people = true;
+      valid_people = false;
+    }else{
+      $('#peoples_'+day+'_0').css('border', '1px solid grey');
+      $('#peoples_'+day+'_1').css('border', '1px solid grey');
+      $('#peoples_'+day+'_2').css('border', '1px solid grey');
+      valid_people = true;
+    }
+    if(valid_people){
+      $('#'+button+'').click();
+    }
+    return valid;
   }
-  if(valid_people){
-    $('#'+button+'').click();
+  else if (action == "update"){
+    var valid = false;
+    if($('#peoples_update_'+day+'_0').val() == '' || $('#peoples_update_'+day+'_1').val() == '' || $('#peoples_update_'+day+'_2').val() == '' || isNaN(parseInt($('#peoples_update_'+day+'_0').val())) || isNaN(parseInt($('#peoples_update_'+day+'_1').val())) || isNaN(parseInt($('#peoples_update_'+day+'_2').val())) || parseInt($('#peoples_update_'+day+'_0').val()) <= 0 || parseInt($('#peoples_update_'+day+'_1').val()) <= 0 || parseInt($('#peoples_update_'+day+'_2').val()) <= 0 ){
+      $('#people_update_warning_text_'+day+'_0').hide();
+      $('#peoples_update_'+day+'_0').css('border', '1px solid red');
+      $('#people_update_warning_text_'+day+'_1').hide();
+      $('#peoples_update_'+day+'_1').css('border', '1px solid red');
+      $('#people_update_warning_text_'+day+'_2').hide();
+      $('#peoples_update_'+day+'_2').css('border', '1px solid red');
+
+      if ($('#peoples_update_'+day+'_0').val() == '' || isNaN(parseInt($('#peoples_update_'+day+'_0').val())) || parseInt($('#peoples_update_'+day+'_0').val()) <= 0 ){
+        $('#people_update_warning_text_'+day+'_0').show();
+        $('#peoples_update_'+day+'_0').css('border', '1px solid red');
+      }
+      else if ($('#peoples_update_'+day+'_1').val() == '' || isNaN(parseInt($('#peoples_update_'+day+'_1').val())) || parseInt($('#peoples_update_'+day+'_1').val()) <= 0 ){
+        $('#people_update_warning_text_'+day+'_1').show();
+        $('#peoples_update_'+day+'_1').css('border', '1px solid red');
+      }
+      else if ($('#peoples_update_'+day+'_2').val() == '' || isNaN(parseInt($('#peoples_update_'+day+'_2').val())) || parseInt($('#peoples_update_'+day+'_2').val()) <= 0 ){
+        $('#people_update_warning_text_'+day+'_2').show();
+        $('#peoples_update_'+day+'_2').css('border', '1px solid red');
+      }
+
+      valid_people = false;
+    }else{
+      $('#peoples_update_'+day+'_0').css('border', '1px solid grey');
+      $('#peoples_update_'+day+'_1').css('border', '1px solid grey');
+      $('#peoples_update_'+day+'_2').css('border', '1px solid grey');
+      valid_people = true;
+    }
+    if(valid_people){
+      $('#'+button+'').click();
+    }
+    return valid;
   }
-  return valid;
+
 }
