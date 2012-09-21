631b4caa2f7cf06211d6ccd9dc25f36928b4a754
dariel <annas_ramadhani@yahoo.co.id>
2012-09-21 18:07:18 +0700

    modified contact page in admin

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -1,3 +1,48 @@
 ActiveAdmin.register Contact do
-  
+
+  actions :all, :except => [:new, :edit]
+
+  filter :name
+  filter :email
+  filter :phone
+  filter :message
+  filter :created_at
+
+  index do
+    column :name
+    column :email
+    column :phone
+    column :message
+    column :created_at
+    default_actions
+  end
+
+  show do |b|
+
+    attributes_table do
+      row :name do
+        b.name
+      end
+      row :email do
+        b.email
+      end
+      row :phone do
+        b.phone
+      end
+      row :message do
+        b.phone
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs "" do
+      f.input :name
+      f.input :email
+      f.input :phone
+      f.input :message
+    end
+    f.buttons
+  end
+
 end
