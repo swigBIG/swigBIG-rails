88d82ba3f56fcb6935b0044781c95b9da30020e8
dariel <annas_ramadhani@yahoo.co.id>
2012-09-27 18:02:38 +0700

    fixed home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,34 +4,33 @@ class HomeController < ApplicationController
   def index;end
 
   def main
-    if is_mobile_view? and !user_signed_in?
-      redirect_to mobile_dashboard_home_url(:mobile)
+    #    if is_mobile_view? and !user_signed_in?
+    #      redirect_to mobile_dashboard_home_url(:mobile)
+    #    else
+
+    @city = City.where(name: @city_lat_lng[0].to_s).first
+    conditions = []
+    conditions << "swigs.swig_type = '#{params[:swig_type]}'" unless params[:swig_type].blank?
+    conditions << "bars.sports_team LIKE '%#{params[:sports_team]}%'" unless params[:sports_team].blank?
+    conditions << "swigs.swig_day = '#{Time.zone.now.strftime("%A").to_s}'"
+    @origin = params[:zip_code].blank? ? [@city_lat_lng[1], @city_lat_lng[2]] : params[:zip_code]
+    unless params[:radius].blank?
+      @bars = Bar.within(params[:radius].to_i, origin: @origin).includes(:swigs).where(conditions.join(" AND "))
     else
-
-
-      @city = City.where(name: @city_lat_lng[0].to_s).first
-      conditions = []
-      conditions << "swigs.swig_type = '#{params[:swig_type]}'" unless params[:swig_type].blank?
-      conditions << "bars.sports_team LIKE '%#{params[:sports_team]}%'" unless params[:sports_team].blank?
-      conditions << "swigs.swig_day = '#{Time.zone.now.strftime("%A").to_s}'"
-      @origin = params[:zip_code].blank? ? [@city_lat_lng[1], @city_lat_lng[2]] : params[:zip_code]
-      unless params[:radius].blank?
-        @bars = Bar.within(params[:radius].to_i, origin: @origin).includes(:swigs).where(conditions.join(" AND "))
-      else
-        #      @bars = Bar.geo_scope(origin: @origin).includes(:swigs).where(conditions.join(" AND "))
-        @bars = Bar.within(25, origin: @origin).includes(:swigs).where(conditions.join(" AND "))
-      end
-      unless params[:zip_code].blank?
-        geo = Geocoder.search("#{params[:zip_code]},#{@city.name}").first
-        @city_lat_lng = [geo.data['city'], geo.data['latitude'], geo.data['longitude']]
-        @bars = Bar.geo_scope(origin: @origin).includes(:swigs).where(conditions.join(" AND ")).where(zip_code: params[:zip_code])
-      end
-
-      respond_to do |format|
-        format.html
-        format.mobile
-      end
+      #      @bars = Bar.geo_scope(origin: @origin).includes(:swigs).where(conditions.join(" AND "))
+      @bars = Bar.within(25, origin: @origin).includes(:swigs).where(conditions.join(" AND "))
     end
+    unless params[:zip_code].blank?
+      geo = Geocoder.search("#{params[:zip_code]},#{@city.name}").first
+      @city_lat_lng = [geo.data['city'], geo.data['latitude'], geo.data['longitude']]
+      @bars = Bar.geo_scope(origin: @origin).includes(:swigs).where(conditions.join(" AND ")).where(zip_code: params[:zip_code])
+    end
+
+    respond_to do |format|
+      format.html
+      format.mobile
+    end
+    #    end
   end
 
   def city
