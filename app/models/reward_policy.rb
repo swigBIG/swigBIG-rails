e7d7bae8eb928c81ad5ba95c4161087d32cc224d
dariel <annas_ramadhani@yahoo.co.id>
2012-10-05 18:21:41 +0700

    fixed reward policy

--- app/models/reward_policy.rb
+++ app/models/reward_policy.rb
@@ -1,5 +1,5 @@
 class RewardPolicy < ActiveRecord::Base
-  attr_accessible :loyalty_expirate_date, :popularity_expirate_hours
+  attr_accessible :loyalty_expirate_date, :popularity_expirate_hours, :expire_within
 
   validates :loyalty_expirate_date, :popularity_expirate_hours, :expire_within, :numericality => true
 end
