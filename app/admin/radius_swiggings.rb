349ee7bc2ec6608538fc170f8a13699853f4705d
dariel <annas_ramadhani@yahoo.co.id>
2012-10-09 17:48:49 +0700

    add radius swiggings to admin

---
+++ app/admin/radius_swiggings.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register RadiusSwigging do
+  menu parent: "Site Radius Setting"
+
+  filter false
+
+  form do |f|
+    f.inputs "to validate for swigging!(note: it will be 1 mile in default)" do
+      f.input :radius
+    end
+    f.buttons
+  end
+
+  controller do
+    def new
+      redirect_to edit_admin_radius_swigging_url(RadiusSwigging.first.id) unless RadiusSwigging.first.blank?
+      @radius_swigging = RadiusSwigging.new
+    end
+  end
+
+end
