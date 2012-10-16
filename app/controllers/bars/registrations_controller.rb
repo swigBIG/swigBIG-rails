15daf72b2889d0b8aa3e0740daeb57b94497a0d0
dariel <annas_ramadhani@yahoo.co.id>
2012-10-16 15:27:12 +0700

    fixed bar show message

--- app/controllers/bars/registrations_controller.rb
+++ app/controllers/bars/registrations_controller.rb
@@ -6,6 +6,7 @@ class Bars::RegistrationsController < Devise::RegistrationsController
   end
 
   def after_sign_up_path_for(resource)
+    session[:request_user_privilage] = true
     #    if resource.created_at.strftime("%v-%R").eql?(resource.updated_at.strftime("%v-%R"))
     if current_bar.address.blank?
       AdminUser.first.send_message(current_bar, {topic: "Welcome to SwigBIG", body: "#{current_bar.name} is not yet listed! <a href='/bars/completion'>Please complete your profile</a>", category: 22})
--- app/controllers/bars/sessions_controller.rb
+++ app/controllers/bars/sessions_controller.rb
@@ -2,19 +2,20 @@ class Bars::SessionsController < Devise::SessionsController
   layout "bars"
 
   def create
+    session[:request_user_privilage] = true
+
     resource = warden.authenticate!(auth_options)
 
     if resource.lock_status.eql?(false)
 
       set_flash_message(:notice, :signed_in) if is_navigational_format?
       sign_in(resource_name, resource)
-
       respond_with resource, :location => after_sign_in_path_for(resource)
       
     elsif resource.lock_status.eql?(true)
 
       redirect_path = after_sign_out_path_for(resource_name)
-#      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+      #      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
       signed_out = sign_out(current_bar)
       flash[:notice] = "Your Account locked!"
 
