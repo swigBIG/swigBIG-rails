aa2f9e9d721173f96c10c5c178f842782474b1c8
dariel <annas_ramadhani@yahoo.co.id>
2012-10-24 17:28:04 +0700

    fixed mobile.js

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -4,10 +4,6 @@
 //= require jquery.mobile
 //= require jquery.easing.compatibility
 //= require bootstrap-tab
-//= require tag-it
-//= require jquery-ui-1.8.autocomplete.min
-//= require jquery-ui.min
-
 
 $(document).ready(function(){
   var lat, lng
