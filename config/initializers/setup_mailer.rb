fe0e47c14ff9b347230b00706003c3c7e833a2da
Angga <angga@41studio.com>
2012-11-21 15:50:41 +0700

    Update config/initializers/setup_mailer.rb

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   port: "587",
   domain: "swigbig.com",
   user_name: "Swigbiginfo@gmail.com",
-  password: 'password4U',
+  password: '',
   authentication: :plain,
   enable_starttls_auto: true
 }
